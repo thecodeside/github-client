5397535176a9de6f6be5a35576932edc9093e1bc
Artur Latoszewski <artur@thecodeside.com>
2021-08-05 22:23:26 +0200

    init2

--- app/src/main/java/com/thecodeside/githubclient/common/utils/ThrowableExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.thecodeside.githubclient.common.utils
-
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
-
-fun Throwable?.isNetworkFailure(): Boolean {
-    return this is SocketTimeoutException ||
-            this is UnknownHostException
-}
--- app/src/main/java/com/thecodeside/githubclient/ui/theme/Dimens.kt
+++
@@ -1,5 +0,0 @@
-package com.thecodeside.githubclient.ui.theme
-
-import androidx.compose.ui.unit.dp
-
-val ProgressSize = 100.dp
